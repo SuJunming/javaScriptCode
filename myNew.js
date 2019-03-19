bff511a3223d3e8b8707018b3f7bd3429097a1ea
苏俊明 <junming.su@qiaofang.local>
2019-03-19 23:06:10 +0800

    feat: add

---
+++ myNew.js
@@ -0,0 +1,7 @@
+function myNew() {
+    let obj = {}
+    let Con = [].shift.call(arguments)
+    obj.__proto__ = Con.prototype
+    let result = Con.apply(obj, arguments)
+    return result instanceof Object ? result : obj
+  }
